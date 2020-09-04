114c68b30304a36fcad97ff09ad13a1e31cda9ed
hyird <31695447+hyird@users.noreply.github.com>
2020-09-04 20:29:14 +0800

    Update custom.sh

--- user/lienol-x64-master/custom.sh
+++ user/lienol-x64-master/custom.sh
@@ -2,3 +2,6 @@
 
 echo "Test custom.sh"
 
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/node-request.git           package/node-request
+

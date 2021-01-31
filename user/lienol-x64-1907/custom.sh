799a02f1d9f4f5605e9c26d29d2c36e4e116966d
hyird <31695447+hyird@users.noreply.github.com>
2021-01-31 11:06:57 +0800

    Update custom.sh

--- user/lienol-x64-1907/custom.sh
+++ user/lienol-x64-1907/custom.sh
@@ -7,4 +7,5 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >>feeds.co
 
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt   package/pdnsd-alt
 

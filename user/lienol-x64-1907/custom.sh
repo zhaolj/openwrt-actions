cc97c9ba11fcc5ce46764fe23b4b954ff4b72821
hyird <31695447+hyird@users.noreply.github.com>
2021-01-31 10:23:54 +0800

    Update custom.sh

--- user/lienol-x64-1907/custom.sh
+++ user/lienol-x64-1907/custom.sh
@@ -4,5 +4,4 @@ echo "Test custom.sh"
 
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt   package/pdnsd-alt
 

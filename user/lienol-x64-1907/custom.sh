1151d38c2ff806cf1ffcc4afda6b5024aea2941e
hyird <31695447+hyird@users.noreply.github.com>
2021-01-31 10:58:33 +0800

    Update custom.sh

--- user/lienol-x64-1907/custom.sh
+++ user/lienol-x64-1907/custom.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 echo "Test custom.sh"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >>feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
